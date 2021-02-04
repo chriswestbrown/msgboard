06bc1c9dc99d695e23ffceffab0c037ec4cdd158
Chris Brown <wcbrown@usna.edu>
2021-02-04 17:43:21 -0500

    debugged minor bugs

--- inst/inst.js
+++ inst/inst.js
@@ -1,7 +1,7 @@
 commandExec["mbconf"] = function(rest) {
     keyboardLockOut = true;
     const http = new XMLHttpRequest();
-    var url = "msg/mbconfig.cgi?opt="+encodeURI(rest);
+    var url = "../msg/mbconfig.cgi?opt="+encodeURI(rest);
     http.onreadystatechange=(e)=>{
 	if (http.readyState == 4 && http.status == 200) {
 	    var resp = http.responseText.trimEnd();
